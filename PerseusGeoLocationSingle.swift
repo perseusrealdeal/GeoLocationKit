ba1104d9ae48e4ca17b37335a0015b5286ed0e0e
Mikhail <mzhigulin@gmail.com>
2023-04-02 19:21:56 +0700

    PUT: PerseusLocationDealer in single
    This commit goes with [PerseusGeoLocationSingle.swift] updated.

--- PerseusGeoLocationSingle.swift
+++ PerseusGeoLocationSingle.swift
@@ -33,9 +33,324 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 //
-// swiftlint_:disable file_length
+// swiftlint:disable file_length
 //
 
-struct GeoLocationService {
-    var text = "Hello, World!"
+import CoreLocation
+
+// Debug servants
+
+#if DEBUG
+let printMessagesInConsole = false
+#endif
+
+// MARK: - Default values
+
+let APPROPRIATE_ACCURACY = LocationAccuracy.threeKilometers
+
+// MARK: - Notifications
+
+extension Notification.Name {
+    static let locationDealerCurrentNotification =
+        Notification.Name("locationDealerCurrentNotification")
+    static let locationDealerUpdatesNotification =
+        Notification.Name("locationDealerUpdatesNotification")
+    static let locationDealerErrorNotification =
+        Notification.Name("locationDealerErrorNotification")
+    static let locationDealerStatusChangedNotification =
+        Notification.Name("locationDealerStatusChangedNotification")
+}
+
+// MARK: - Errors
+
+enum LocationDealerError: Error, Equatable {
+    case receivedEmptyLocationData
+    case failedRequest(String)
+}
+
+// MARK: - Business class
+
+class PerseusLocationDealer: NSObject {
+
+    // MARK: - Difficult Dependencies
+
+    private var locationManager: CLLocationManager
+    private var notificationCenter: NotificationCenter
+
+    // MARK: - Calculated Properties
+
+    var desiredAccuracy: CLLocationAccuracy { return locationManager.desiredAccuracy }
+
+    var authorizationStatus: CLAuthorizationStatus { return authorizationStatusHidden }
+    private var authorizationStatusHidden: CLAuthorizationStatus {
+        return type(of: locationManager).authorizationStatus()
+    }
+
+    var locationServicesEnabled: Bool { return locationServicesEnabledHidden }
+    private var locationServicesEnabledHidden: Bool {
+        return type(of: locationManager).locationServicesEnabled()
+    }
+
+    var locationPermit: LocationDealerPermit { return locationPermitHidden }
+    private var locationPermitHidden: LocationDealerPermit {
+        return getPermit(serviceEnabled: locationServicesEnabledHidden,
+                         status: authorizationStatusHidden)
+    }
+
+    // Internal Flags
+
+    internal var currentLocationDealOnly: Bool = false
+
+    // MARK: - Singleton constructor
+
+    static let shared: PerseusLocationDealer = { return PerseusLocationDealer() }()
+
+    private override init() {
+
+        #if DEBUG
+        if printMessagesInConsole { print(">> [\(type(of: self))]." + #function) }
+        #endif
+
+        self.locationManager = CLLocationManager()
+        self.notificationCenter = NotificationCenter.default
+
+        super.init()
+
+        locationManager.desiredAccuracy = APPROPRIATE_ACCURACY.rawValue
+        locationManager.delegate = self
+    }
+
+    // MARK: - Contract
+
+    func askForCurrentLocation(
+        accuracy: LocationAccuracy = APPROPRIATE_ACCURACY,
+        _ actionIfNotAllowed: ((_ permit: LocationDealerPermit) -> Void)? = nil) {
+
+        #if DEBUG
+        if printMessagesInConsole { print(">> [\(type(of: self))]." + #function) }
+        #endif
+
+        let permit = locationPermitHidden
+
+        guard permit == .allowed else { actionIfNotAllowed?(permit); return }
+
+        locationManager.stopUpdatingLocation()
+
+        currentLocationDealOnly = true
+        locationManager.desiredAccuracy = accuracy.rawValue
+
+        locationManager.startUpdatingLocation()
+    }
+
+    #if os(iOS)
+    func askForAuthorization(_ authorization: LocationAuthorization) {
+
+        #if DEBUG
+        if printMessagesInConsole { print(">> [\(type(of: self))]." + #function) }
+        #endif
+
+        switch authorization {
+        case .whenInUse:
+            locationManager.requestWhenInUseAuthorization()
+        case .always:
+            locationManager.requestAlwaysAuthorization()
+        }
+    }
+    #endif
+
+    func askToStartUpdatingLocation(accuracy: LocationAccuracy = APPROPRIATE_ACCURACY) {
+
+        #if DEBUG
+        if printMessagesInConsole { print(">> [\(type(of: self))]." + #function) }
+        #endif
+
+        currentLocationDealOnly = false
+        locationManager.stopUpdatingLocation()
+
+        locationManager.desiredAccuracy = accuracy.rawValue
+        locationManager.startUpdatingLocation()
+    }
+
+    func askToStopUpdatingLocation() {
+
+        #if DEBUG
+        if printMessagesInConsole { print(">> [\(type(of: self))]." + #function) }
+        #endif
+
+        currentLocationDealOnly = false
+        locationManager.stopUpdatingLocation()
+    }
+}
+
+// MARK: - CLLocationManagerDelegate
+
+extension PerseusLocationDealer: CLLocationManagerDelegate {
+
+    func locationManager(_ manager: CLLocationManager,
+                         didChangeAuthorization status: CLAuthorizationStatus) {
+        #if DEBUG
+        if printMessagesInConsole { print(">> [\(type(of: self))]." + #function) }
+        #endif
+
+        notificationCenter.post(name: .locationDealerStatusChangedNotification, object: status)
+    }
+
+    func locationManager(_ manager: CLLocationManager,
+                         didFailWithError error: Error) {
+        #if DEBUG
+        if printMessagesInConsole { print(">> [\(type(of: self))]." + #function) }
+        #endif
+
+        currentLocationDealOnly = false
+        locationManager.stopUpdatingLocation()
+
+        let result: LocationDealerError = .failedRequest(error.localizedDescription)
+
+        notificationCenter.post(name: .locationDealerErrorNotification, object: result)
+    }
+
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation]) {
+        #if DEBUG
+        if printMessagesInConsole { print(">> [\(type(of: self))]." + #function) }
+        #endif
+
+        guard !currentLocationDealOnly else {
+
+            currentLocationDealOnly = false
+            locationManager.stopUpdatingLocation()
+
+            let result: Result<CLLocation, LocationDealerError> =
+                locations.first != nil ?
+                    .success(locations.first!) :
+                    .failure(.receivedEmptyLocationData)
+
+            notificationCenter.post(name: .locationDealerCurrentNotification, object: result)
+            return
+        }
+
+        let result: Result<[CLLocation], LocationDealerError> =
+            !locations.isEmpty ? .success(locations) : .failure(.receivedEmptyLocationData)
+
+        notificationCenter.post(name: .locationDealerUpdatesNotification, object: result)
+    }
+}
+
+// MARK: - Data structures and functions used in library
+
+enum Result<Value, Error: Swift.Error> {
+    case success(Value)
+    case failure(Error)
+}
+
+struct LocationAccuracy: RawRepresentable, Equatable {
+    var rawValue: CLLocationAccuracy
+
+    // The highest possible accuracy that uses additional sensor data.
+    static let bestForNavigation =
+        LocationAccuracy(rawValue: kCLLocationAccuracyBestForNavigation)
+
+    // The best level of accuracy available.
+    static let best = LocationAccuracy(rawValue: kCLLocationAccuracyBest)
+
+    // Accurate to within ten meters of the desired target.
+    static let nearestTenMeters =
+        LocationAccuracy(rawValue: kCLLocationAccuracyNearestTenMeters)
+
+    // Accurate to within one hundred meters.
+    static let hundredMeters = LocationAccuracy(rawValue: kCLLocationAccuracyHundredMeters)
+
+    // Accurate to the nearest kilometer.
+    static let kilometer = LocationAccuracy(rawValue: kCLLocationAccuracyKilometer)
+
+    // Accurate to the nearest three kilometers.
+    static let threeKilometers = LocationAccuracy(rawValue: kCLLocationAccuracyThreeKilometers)
+
+}
+
+extension CLAuthorizationStatus: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .notDetermined:
+            return "notDetermined"
+        case .restricted:
+            return "restricted"
+        case .denied:
+            return "denied"
+        case .authorizedAlways:
+            return "authorizedAlways"
+        case .authorizedWhenInUse: // iOS
+            return "authorizedWhenInUse"
+        }
+    }
+}
+
+#if os(iOS)
+enum LocationAuthorization: CustomStringConvertible {
+
+    case whenInUse
+    case always
+
+    var description: String {
+        switch self {
+        case .whenInUse:
+            return "When-in-use"
+        case .always:
+            return "Always"
+        }
+    }
+}
+#endif
+
+enum LocationDealerPermit: CustomStringConvertible {
+    /// Location service is neither restricted nor the app denided
+    case notDetermined
+
+    /// provide instructions for changing restrictions options in
+    /// Settings > General > Restrictions
+    case deniedForAllAndRestricted /// in case if location services turned off
+    case restricted  /// in case if location services turned on
+
+    /// provide instructions for enabling the Location Services switch in Settings > Privacy
+    case deniedForAllApps /// in case if location services turned off but not restricted
+
+    /// provide instructions for enabling services for the app in Settings > The App
+    case deniedForTheApp /// in case if location services turned on but not restricted
+
+    /// either authorizedAlways or authorizedWhenInUse
+    case allowed
+
+    var description: String {
+        switch self {
+        case .notDetermined:
+            return "notDetermined"
+        case .deniedForAllAndRestricted:
+            return "deniedForAllAndRestricted"
+        case .restricted:
+            return "restricted"
+        case .deniedForAllApps:
+            return "deniedForAllApps"
+        case .deniedForTheApp:
+            return "deniedForTheApp"
+        case .allowed:
+            return "allowed"
+        }
+    }
+}
+
+func getPermit(serviceEnabled: Bool, status: CLAuthorizationStatus) -> LocationDealerPermit {
+
+    if status == .notDetermined {
+        return .notDetermined
+    }
+
+    if status == .denied {
+        return serviceEnabled ? .deniedForTheApp : .deniedForAllApps
+    }
+
+    if status == .restricted {
+        return serviceEnabled ? .restricted : .deniedForAllAndRestricted
+    }
+
+    return .allowed
 }
